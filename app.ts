d3e1a5d43748f8bcd50933713cb0c0ed57089044
Muhammed Shaheer <ckshaheer2012@gmail.com>
2021-04-02 21:58:12 +0530

    arrays and enum implemented in ts

--- app.ts
+++ app.ts
@@ -7,3 +7,22 @@ var k: Number = 101;
 console.log(a);
 
 enum LanguageKnown { English, Hindi, Malayalam };
+
+let student = {
+    Name: "Karthik",
+    Age: 30,
+    PhoneNumber: 9847012345,
+    Language: LanguageKnown[LanguageKnown.Hindi]
+};
+
+let studentsList = [
+    { Name: "Prasanth", Age: 26, PhoneNumber: 9995012345, Language: LanguageKnown[LanguageKnown.English] }
+];
+
+studentsList.push(student);
+
+for (let index = 0; index < studentsList.length; index++) {
+    const element = studentsList[index];
+
+    console.log("Age:" + element.Age + " with Name:" + element.Name + " has Phone:" + element.PhoneNumber + " speaks:" + element.Language);
+}
