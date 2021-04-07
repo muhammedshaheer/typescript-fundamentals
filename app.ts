5a20c860c1aa722a86e7bd1c2686380d4d9d8190
Muhammed Shaheer <ckshaheer2012@gmail.com>
2021-04-07 12:09:05 +0530

    working with functions

--- app.ts
+++ app.ts
@@ -6,6 +6,7 @@ var k: Number = 101;
 
 console.log(a);
 
+//Enums
 enum LanguageKnown { English, Hindi, Malayalam };
 
 let student = {
@@ -15,14 +16,41 @@ let student = {
     Language: LanguageKnown[LanguageKnown.Hindi]
 };
 
+//Arrays
 let studentsList = [
     { Name: "Prasanth", Age: 26, PhoneNumber: 9995012345, Language: LanguageKnown[LanguageKnown.English] }
 ];
 
 studentsList.push(student);
 
-for (let index = 0; index < studentsList.length; index++) {
-    const element = studentsList[index];
+// for (let index = 0; index < studentsList.length; index++) {
+//     const element = studentsList[index];
 
-    console.log("Age:" + element.Age + " with Name:" + element.Name + " has Phone:" + element.PhoneNumber + " speaks:" + element.Language);
-}
+//     console.log("Age:" + element.Age + " with Name:" + element.Name + " has Phone:" + element.PhoneNumber + " speaks:" + element.Language);
+// }
+
+//Function
+function GetStudentsList(students: any[]) {
+    students.forEach(element => {
+        console.log("Age:" + element.Age + " with Name:" + element.Name + " has Phone:" + element.PhoneNumber + " speaks:" + element.Language);
+    })
+};
+
+GetStudentsList(studentsList);
+
+//Function Rest Parameters
+function GetNumbers(...nums: Number[]) {
+    nums.forEach(element => {
+        console.log("Number: " + element);
+    });
+};
+
+GetNumbers(1, 2, 3, 4, 5, 6);
+
+//Default Parameters
+function GetInfo(info: string = "Happy") {
+    console.log(info);
+};
+
+GetInfo();
+GetInfo("Very Happy");
